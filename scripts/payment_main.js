01e62cd8f9e810b332b5947b546558bb7690cd70
fahad alsowaylim <36514484+fss88@users.noreply.github.com>
2021-03-31 22:34:28 -0700

    Added the finished HW

---
+++ scripts/payment_main.js
@@ -0,0 +1,30 @@
+(function(window) {
+  "use strict";
+  /* eslint-disable no-console */
+
+  var PAYMENT_FORM_SELECTOR = "[payment-order='form']";
+  var POPUP_SELECTOR = "[data-payment-modal='popup']";
+
+  var App = window.App;
+  var Payment = App.Payment;
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var PopUp = App.PopUp;
+  var $ = window.jQuery;
+
+  var myPayment = new Payment(new DataStore());
+  window.myPayment = myPayment;
+  
+  var popUp = new PopUp(POPUP_SELECTOR);
+  var paymentFormHandler = new FormHandler(PAYMENT_FORM_SELECTOR);
+
+  // payment POP UP window 
+
+  paymentFormHandler.addSubmitHandler(function(data) {
+    myPayment.createPayment.call(myPayment, data);
+    popUp.addInfo.call(popUp, data);
+    $("#payWindow").modal();
+  });
+
+  console.log(paymentFormHandler);
+})(window);
